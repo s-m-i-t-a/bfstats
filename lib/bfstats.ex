cb15b4b8b7fc66e6307c520769cecf063218bc00
Jindrich K. Smitka <jsmitka@smita.info>
2017-06-04 22:31:38 +0200

    Init...

---
+++ lib/bfstats.ex
@@ -0,0 +1,18 @@
+defmodule Bfstats do
+  @moduledoc """
+  Documentation for Bfstats.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Bfstats.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/bfstats_test.exs
@@ -0,0 +1,8 @@
+defmodule BfstatsTest do
+  use ExUnit.Case
+  doctest Bfstats
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
