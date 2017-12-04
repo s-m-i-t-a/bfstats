971dfde7ee393a8ead8362d0982975587a93b08c
Jindrich K. Smitka <jsmitka@smita.info>
2017-12-04 13:10:14 +0100

    Added csv, result, timex packages

--- mix.exs
+++ mix.exs
@@ -5,6 +5,7 @@ defmodule Bfstats.Mixfile do
     [app: :bfstats,
      version: "0.1.0",
      elixir: "~> 1.4",
+     escript: escript_config(),
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps()]
@@ -15,7 +16,8 @@ defmodule Bfstats.Mixfile do
   # Type "mix help compile.app" for more information
   def application do
     # Specify extra applications you'll use from Erlang/Elixir
-    [extra_applications: [:logger]]
+    # [extra_applications: [:logger]]
+    [applications: [:logger]]
   end
 
   # Dependencies can be Hex packages:
@@ -28,6 +30,16 @@ defmodule Bfstats.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [
+      {:csv, "~> 2.0.0"},
+      {:timex, "~> 3.1"},
+      {:result, "~> 1.1"},
+      {:dogma, "~> 0.1", only: :dev},
+      {:credo, "~> 0.8", only: [:dev, :test], runtime: false},
+    ]
+  end
+
+  defp escript_config do
+    [main_module: Bfstats.CLI]
   end
 end
