964234c8c388297887adde7d7edfad75d22a4617
Jindrich K. Smitka <jsmitka@smita.info>
2017-12-04 15:54:06 +0100

    Added agragated time for project

--- lib/bfstats/compute.ex
+++ lib/bfstats/compute.ex
@@ -3,11 +3,17 @@ defmodule Bfstats.Compute do
   Some computations on tasks list
   """
 
+  def group(stream) do
+    stream
+    |> Enum.to_list()
+    |> Enum.group_by(fn([_, _, _, project, _]) -> project end)
+  end
+
   def from(stream, date) do
     stream
     |> Stream.reject(
       fn([task_date | _]) ->
-        Timex.compare(task_date, to_date(date), :days) == -1
+        Timex.compare(task_date, to_date(date)) == -1
       end
     )
   end
@@ -21,8 +27,26 @@ defmodule Bfstats.Compute do
     )
   end
 
+  def sum_project_duration(map) do
+    map
+    |> Enum.map(
+      fn({project, tasks}) ->
+         {
+           project,
+           tasks
+           |> Enum.reduce(0, fn([_, time, _, _, _], acc) -> acc + time end)
+           |> to_hours()
+         }
+      end
+    )
+  end
+
   defp to_date(date) do
     date
     |> Timex.parse!("%Y-%m-%d", :strftime)
+  end
+
+  defp to_hours(minutes) do
+    minutes / 60
   end
 end
